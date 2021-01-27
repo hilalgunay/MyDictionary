ef74a46c42482c16b3f9c76e8f8fef4dfdd745e3
Hilal Günay <hilallgunay@hotmail.com>
2021-01-27 03:27:58 +0300

    Add project files.

---
+++ MyList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionay<int, string> myDictionary = new MyDictionay<int, string>();
+
+
+            myDictionary.Add(1, "Muğla");
+            myDictionary.Add(2, "Bursa");
+        }
+
+    }
+}
